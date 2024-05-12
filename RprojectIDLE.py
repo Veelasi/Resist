bbade997a3099b12cf1d98d5fc279eb6f6868da5
Veelasi <krekerok45@gmail.com>
2024-05-13 02:34:39 +0400

    Add files via upload

---
+++ RprojectIDLE.py
@@ -0,0 +1,26 @@
+import os
+#os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+while 0 < 1:
+    print("Rproject.inc")
+    print("Что будем считать?")
+    print("1 - Сопротивление резистора для светодиода")
+    print("2 - Частота колебательного контура")
+    print("3 - Расчет частоты исходя из волны")
+    print("4 - Индуктивность катушки по намотанный данным")
+    print("5 - Удельное сопротивление проводника")
+    print("6 - Удельное сопротивление проводника по нагреву (< 200 C)")
+    a = int(input())
+    if a == 1:
+        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+    if a == 2:
+        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+    if a == 3:
+        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+    if a == 4:
+        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+    if a == 5:
+        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+    if a == 6:
+        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+    else:
+        print("В диапазоне чисел выше укажи число")
