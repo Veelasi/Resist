496568528ef579a6b946691229c76624886a2de1
Veelasi <krekerok45@gmail.com>
2024-05-14 08:22:27 +0400

    Add files via upload

--- RprojectIDLE.py
+++ RprojectIDLE.py
@@ -1,5 +1,5 @@
 import os
-#os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+#os.startfile(r'C:\Rproject\ResistV1.py')
 while 0 < 1:
     print("Rproject.inc")
     print("Что будем считать?")
@@ -11,16 +11,16 @@ while 0 < 1:
     print("6 - Удельное сопротивление проводника по нагреву (< 200 C)")
     a = int(input())
     if a == 1:
-        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+        os.startfile(r'C:\Rproject\ResistV1.py')
     if a == 2:
-        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+        os.startfile(r'C:\Rproject\Chastota.py')
     if a == 3:
-        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+        os.startfile(r'C:\Rproject\Volna.py')
     if a == 4:
-        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+        os.startfile(r'C:\Rproject\katushka.py')
     if a == 5:
-        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+        os.startfile(r'C:\Rproject\udelnoe.py')
     if a == 6:
-        os.startfile(r'C:\Users\valera\Desktop\Rproject\ResistV1.py')
+        os.startfile(r'C:\Rproject\nagrev.py')
     else:
         print("В диапазоне чисел выше укажи число")
